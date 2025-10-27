8c2102711fb5418712fe30d68f9504b009decb68
Gabor Monostori <monogab@gmail.com>
2025-10-27 15:47:05 +0100

    generated factry project with certificate

---
+++ common/src/main/java/io/factry/historian/common/FactryHistorianModule.java
@@ -0,0 +1,6 @@
+package io.factry.historian.common;
+
+public class FactryHistorianModule {
+    public static final String MODULE_ID = "io.factry.historian.FactryHistorian";
+
+}
---
+++ docs/content.md
@@ -0,0 +1,9 @@
+# Factry Historian Module
+
+
+## Meeting Notes:
+ 1. [Kick-off meeting](meeting_notes1)
+
+
+## Content:
+ 1. [Plan](plan.md)
