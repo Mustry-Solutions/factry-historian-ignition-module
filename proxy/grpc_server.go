5ba8c9e15430188d5c7f627b0f4f7117ac8f4721
Gabor Monostori <gabor.monostori@mustrysolutions.com>
2026-02-24 16:19:38 +0100

    grp_server human readable format output

--- proxy/grpc_server.go
+++ proxy/grpc_server.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"log"
+	"time"
 
 	pb "factry-historian-proxy/proto/historianpb"
 )
@@ -17,8 +18,9 @@ func (s *historianServer) Store(ctx context.Context, req *pb.StoreRequest) (*pb.
 	log.Printf("[gRPC] Received StoreRequest with %d samples", count)
 
 	for i, sample := range req.GetSamples() {
-		log.Printf("[gRPC]   sample[%d]: tag_path=%s timestamp_ms=%d value_int=%d value_double=%f quality=%d",
-			i, sample.GetTagPath(), sample.GetTimestampMs(),
+		ts := time.UnixMilli(sample.GetTimestampMs()).UTC().Format("2006-01-02 15:04:05.000")
+		log.Printf("[gRPC]   sample[%d]: tag=%s  time=%s  value_int=%d  value_double=%f  quality=%d",
+			i, sample.GetTagPath(), ts,
 			sample.GetValueInt(), sample.GetValueDouble(), sample.GetQuality())
 	}
 
